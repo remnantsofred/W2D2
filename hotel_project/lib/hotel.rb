f04cb48c23b8a69504e0de8b7e165930d1f7df4f
remnantsofred <remnantsofred@gmail.com>
2022-10-11 14:10:42 -0700

    finished hotel project

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,56 @@
 require_relative "room"
 
 class Hotel
-  
+    attr_accessor :rooms
+
+    def initialize(name, room_info)
+        @name = name
+        @rooms = {}        
+        room_info.each do |room_name, capacity|
+            @rooms[room_name] = Room.new(capacity) 
+        end
+    end
+
+    def name
+        names = @name.split(" ")
+        capitalized = names.map { |name| name[0].upcase + name[1..-1].downcase }
+        capitalized.join(" ")
+    end
+
+    def room_exists?(room_name)
+        if @rooms.has_key?(room_name)
+            return true
+        else 
+            return false
+        end
+    end
+
+    def check_in(occupant, room_name)
+        if !room_exists?(room_name)
+            print 'sorry, room does not exist'
+        else
+            if @rooms[room_name].add_occupant(occupant)
+                print 'check in successful'
+            else
+                print 'sorry, room is full'
+            end
+        end
+    end
+
+    def has_vacancy?
+        @rooms.each_key do | room_name | 
+            if !@rooms[room_name].full?
+                return true
+            end
+        end
+        false
+    end
+
+    def list_rooms
+        @rooms.each_key do | room_name |
+            print room_name + ".*" + @rooms[room_name].available_space.to_s
+            puts
+        end
+    end   
+
 end
--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
+    attr_accessor :capacity, :occupants
+    
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
   
+    def full?
+        return false if @occupants.length < @capacity
+        return true if @occupants.length == @capacity
+    end
+
+    def available_space
+        @capacity - @occupants.length
+    end
+
+    def add_occupant(occupant)
+        if full?
+            return false
+        else
+            @occupants << occupant
+            return true
+        end
+    end
+
 end
